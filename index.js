e7f67fd31911b3f424230ef6229ce0f6399666b5
danielmorbeck <danielmorbeck@gmail.com>
2023-09-15 19:13:25 -0300

    BE: implemented post /tasks route

--- index.js
+++ index.js
@@ -7,13 +7,24 @@ const PORT = process.env.PORT || 8000;
 app.use(bodyParser.json());
 
 let tasks = [];
-let nextTaskId = 1;
+let nextTaskId = 0;
 
 app.get('/tasks', (req, res) => {
+  res.status(200);
   res.json(tasks);
 });
 
 app.post('/tasks', (req, res) => {
+  const newTask = {
+    id: nextTaskId + 1,
+    title: req.body.title,
+    description: req.body.description,
+  };
+
+  tasks.push(newTask);
+  nextTaskId++;
+  res.status(200)
+  res.json(newTask)
 });
 
 
