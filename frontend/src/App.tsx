9d2089224bba08e7ba003f83508621dcb0d549c8
danielmorbeck <danielmorbeck@gmail.com>
2023-09-17 10:23:57 -0300

    FE: handle empty fields errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,10 +6,19 @@ type Task = {
   description: string
 }
 
+type ApiResponse<T> = {
+  success: boolean,
+  status: number,
+  data?: T,
+  message?: string,
+}
+
 function App() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [formData, setFormData] = useState({ title: '', description: '' });
+  const [formDataError, setFormDataError] = useState({message: ''});
   const [displayUpdateForm, setDisplayUpdateForm] = useState(false);
+  const [updateFormError, setUpdateFormError] = useState({message: ''});
   const [updatedTask, setUpdatedTask] = useState({id: 0, title: '', description: ''});
 
   useEffect(() => {
@@ -24,25 +33,57 @@ function App() {
 
   /* Complete the following functions to hit endpoints on your server */
   const createTask = async () => {
-    await fetch('http://localhost:8000/tasks', { 
-      headers: {
-        "Content-Type": "application/json",
-      }, 
-      method: 'POST', 
-      body: JSON.stringify({'title': formData.title, 'description': formData.description})
-    });
-    await fetchTasks();
+    try {
+      const response = await fetch('http://localhost:8000/tasks', { 
+        headers: {
+          "Content-Type": "application/json",
+        }, 
+        method: 'POST', 
+        body: JSON.stringify({'title': formData.title, 'description': formData.description})
+      });
+      const responseData: ApiResponse<Task> = await response.json();
+
+      if (responseData.success) {
+        await fetchTasks();
+        setFormDataError({message: ''});
+        return;
+      }
+
+      if (responseData.status === 422) {
+        setFormDataError({message: responseData.message || 'Something went wrong'})
+        return;
+      }
+      
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   const updateTask = async (id: number) => {
-    await fetch(`http://localhost:8000/tasks/${id}`, { 
-      headers: {
-        "Content-Type": "application/json",
-      }, 
-      method: 'PUT', 
-      body: JSON.stringify({'title': updatedTask.title, 'description': updatedTask.description})
-    });
-    await fetchTasks();
+    try {
+      const response = await fetch(`http://localhost:8000/tasks/${id}`, { 
+        headers: {
+          "Content-Type": "application/json",
+        }, 
+        method: 'PUT', 
+        body: JSON.stringify({'title': updatedTask.title, 'description': updatedTask.description})
+      });
+
+      const responseData: ApiResponse<Task> = await response.json();
+
+      if (responseData.success) {
+        await fetchTasks();
+        setUpdateFormError({message: ''});
+        return;
+      }
+
+      if (responseData.status === 422) {
+        setUpdateFormError({message: responseData.message || 'Something went wrong'})
+        return;
+      }
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   const deleteTask = async (id: number) => {
@@ -85,7 +126,8 @@ function App() {
             value={updatedTask.description}
             onChange={e => setUpdatedTask({ ...updatedTask, description: e.target.value })}
           />
-          <button onClick={() => updateTask(updatedTask.id)}>Create</button>
+          <button onClick={() => updateTask(updatedTask.id)}>Update</button>
+          <p>{updateFormError.message}</p>
         </div>
         }
       </ul>
@@ -103,6 +145,7 @@ function App() {
           value={formData.description}
           onChange={e => setFormData({ ...formData, description: e.target.value })}
         />
+        <p>{formDataError.message}</p>
         <button onClick={createTask}>Create</button>
       </div>
     </div>
